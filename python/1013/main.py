502653f3034962817327f66d561dbe4129c03ee5
GlauberBalsani <glauber_29@hotmail.com>
2021-12-07 20:41:49 -0300

    maneira lógica 1013

---
+++ python/1013/main.py
@@ -0,0 +1,15 @@
+sami = input().split()
+a = int(sami[0])
+b = int(sami[1])
+c = int(sami[2])
+maior = 0
+
+if a>b and a>c:
+    maior = a
+elif b>a and b>c:
+    maior = b
+else:
+    maior = c
+
+print(f"{maior} eh o maior")    
+
