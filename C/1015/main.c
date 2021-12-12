1e207bd1aba8cf9686d2a4e1996c7b0c81b8177e
GlauberBalsani <glauber_29@hotmail.com>
2021-12-12 16:46:33 -0300

    fazendo em C

---
+++ C/1015/main.c
@@ -0,0 +1,42 @@
+
+/* 
+ 
+
+Leia os quatro valores correspondentes aos eixos x e y de dois pontos quaisquer no plano,
+p1(x1,y1) e p2(x2,y2) e calcule a distância entre eles,
+mostrando 4 casas decimais após a vírgula, segundo a fórmula:
+
+
+Entrada
+
+O arquivo de entrada contém duas linhas de dados.
+A primeira linha contém dois valores de ponto flutuante: x1 y1
+e a segunda linha contém dois valores de ponto flutuante x2 y2.
+Saída
+
+Calcule e imprima o valor da distância segundo a fórmula fornecida, com 4 casas após o ponto decimal.
+
+
+
+
+
+*/
+
+
+
+
+
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+	float x1, y1, x2 ,y2, distancia;
+	scanf("%f%f", &x1,&y1);
+	scanf("%f%f", &x2,&y2);
+	distancia = sqrtf(pow (x2 - x1,2) + pow (y2 - y1,2)) ;// na hora de compliar passar a flag -lm
+	printf("%.4f\n",distancia);
+	
+
+}
